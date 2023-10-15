028c6b30a40d0b70a490e2636f419e755569dbd4
Sarah Shokoor <sarahshokoor@gmail.com>
2023-10-14 22:09:01 -0700

    add img folder, logo edited main.py, index.html, contact.html

--- README.md
+++ README.md
@@ -4,13 +4,16 @@ Website for Logistics company
 
 ## To Dos
 
-- [ ] base site... header / footer, structure of app
-  - [ ] Home - https://schema.org/LocalBusiness
-  - [ ] Contact - https://chat.openai.com/share/d21b0178-697d-469a-ac7f-639d2ccbdcd2
-  - [ ] Bootstrap styling example
-  - [ ] upsplash stock photos for box trucks
+- [x] base site... header / footer, structure of app
+  - [x] Home - https://schema.org/LocalBusiness
+  - [x] Contact - https://chat.openai.com/share/d21b0178-697d-469a-ac7f-639d2ccbdcd2
+  - [x] Bootstrap styling example
+  - [x] upsplash stock photos for box trucks
 - [x] humans.txt
 - [ ] Google Form - embed link iframe
 - [ ] Google Business
+- [ ] Edit Contact Form on index.html - Add email link when Google Business is set up 
 - [ ] Google Phone Number
+- [ ] Google Analytics
+- [ ] Google Translate
 - [ ] Domain
--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from jinja2 import Template, Environment, FileSystemLoader
 
 company_name = "Abby's Freight Logistics"
+company_number = '925-727-5991'
+company_address = '2223 Mount Pellier St'
+company_city = 'Tracy'
+company_state = 'CA'
+company_zip = '95304'
 
 file_loader = FileSystemLoader('src/templates')
 env = Environment(loader=file_loader)
@@ -8,8 +13,10 @@ env = Environment(loader=file_loader)
 
 def process_html(file_name):
     content = open(f'src/{file_name}', 'r').read()
+    content_template = Template(content)
+    content_output = content_template.render(company_name=company_name, company_number=company_number, company_address=company_address, company_city=company_city, company_state=company_state, company_zip=company_zip)
     template = env.get_template('index.html')
-    output = template.render(company_name=company_name, content=content)
+    output = template.render(company_name=company_name, company_number=company_number, company_address=company_address, company_city=company_city, company_state=company_state, company_zip=company_zip, content=content_output)
     with open(f'docs/{file_name}', "w") as file:
         file.write(output)
 
